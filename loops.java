cfead64b31125ff0c53eec57ecc3fef1ecb2ece3
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-08 16:05:46 +0530

    updates

---
+++ loops.java
@@ -0,0 +1,39 @@
+
+// for,while are entry controlled loops
+//do while is exi controlled loop.
+
+
+public class loops {
+    public static void main (String[] args)
+    {
+        // // nested while loops
+        // int i =0;
+        // while (i < 5 )
+        // {   
+        //     i++;
+        //     System.out.println("Hi" +" " + i);
+
+        //     int j =0;
+        //     while (j <=3)
+        //     {
+        //         j++;
+        //         System.out.println("Hello" + " " + j);
+        //     }
+        // }
+
+        // System.out.println("bye" + i);
+
+
+
+            // do while loops
+
+            int i =0;
+            do{
+                
+                System.out.println("Hi"+ i);
+                i++;
+
+            }while(i<=4);
+
+    }    
+}
