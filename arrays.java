cf107e24776987f20d259428767381a5a341daf5
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-10 17:15:03 +0530

    updates

--- arrays.java
+++ arrays.java
@@ -16,13 +16,14 @@ public class arrays
          }
 
 
-        int arr1 [][] = new int[3][4]; //multi-dimension array
+         //multi-dimension array
+        int arr1 [][] = new int[3][4]; //2D-dimension array
 
          for(int i=0;i<arr1.length;i++)
          {
             for(int j=0;j<arr1[i].length;j++)  //for iterating through the columns
             {
-                arr1[i] [j] =  (int) (Math.random() * 100); // putting random numbers in the array
+                arr1[i] [j] =  (int) (Math.random() * 10); // putting random numbers in the array
                 //System.out.println(arr1[i][j]);
             }
          }
@@ -35,6 +36,59 @@ public class arrays
             }
             System.out.println();
          }
+          System.out.println();
+
+
+         //enhanced for loop for printing same as above numbers
+
+         for(int n[]:arr1)
+         {
+            for(int m:n)
+            {
+                System.out.print( m + " ");
+            }
+            System.out.println("Enhanced for loop ");
+         }
+         System.out.println();
+
+         // 3-D array
+         int darr[][][] = new int[2][3][4];
+
+         
+         
+
+
+
+
+         // jagged array
+
+         //different number of columns in array
+
+         int jagg[][] = new int[3][]; //jagged array
+
+         jagg[0] = new int[3];
+         jagg[1] = new int[2];
+         jagg[2] = new int[4];
+        
+          for(int i=0;i<jagg.length;i++)
+         {
+            for(int j=0;j<jagg[i].length;j++)  //for iterating through the columns
+            {
+                jagg[i] [j] =  (int) (Math.random() * 10); // putting random numbers in the array
+                //System.out.println(arr1[i][j]);
+            }
+         }
+
+        for(int n[]:jagg)
+         {
+            for(int m:n)
+            {
+                System.out.print( m + " ");
+            }
+            System.out.println("jagged array ");
+         }
+
+
     }
   
 }
