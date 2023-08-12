28087c28dcfe29d08b4626470c14a369962bca2f
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-12 15:14:48 +0530

    updates

--- strings.java
+++ strings.java
@@ -1,8 +1,37 @@
 public class strings {
-    public static void main(strings args[])
+    public static void main(String args[])
     {
         String name = "dev";
         System.out.println((name));
+        System.out.println((name.charAt(0)));
+        System.out.println((name.concat("patil")));
+
+
+        // strings are immutable by nature and cannot be changed once created
+
+        String name1 = "dev";
+        System.out.println(name==name1); // because to name1 object the address of name object is sent
+
+        name1 = name1 + "patil"; // this will create new object with new address in heap memory
+
+        System.out.println(name1);
+
+
+        // String Buffer is mutable where we can change it also its thread safe.
+
+
+        StringBuffer sb = new StringBuffer("");
+        System.out.println(sb.capacity()); // the Stringbuffer capacity is 16
+
+        StringBuffer sb1 = new StringBuffer("shiv");
+        System.out.println(sb1.hashCode());  //hashcode
+        System.out.println(sb1.deleteCharAt(1)); // deleting character at specific index
+        System.out.println(sb1.append("Dev"));
+
+
+
+
+
     }
     
 }
