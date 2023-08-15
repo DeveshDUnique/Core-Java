378a5487fd7845e4de6915f437903dc3648b9ce8
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-15 15:49:05 +0530

    updates

---
+++ demo1.java
@@ -0,0 +1,22 @@
+
+import Access.A;
+
+
+
+
+public class demo1 
+{
+    public static void main(String args[])
+    {
+
+
+        A obj = new A();
+        System.out.println(obj.marks);
+
+        // System.out.println(obj.id); // protected variables
+        // System.out.println(obj.roll_no); // private variables
+
+        obj.show();
+
+    }
+}
